3c983c40ce868fbe48c6c66b002436acd6857e12
Bhaskar_Jyoti_Dutta <bhaskarduttapersonal@gmail.com>
2023-07-16 09:41:04 +0530

    Project Complete

--- app/src/main/java/com/bhaskar/livedata/MainActivity.kt
+++ app/src/main/java/com/bhaskar/livedata/MainActivity.kt
@@ -2,6 +2,9 @@ package com.bhaskar.livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
@@ -9,16 +12,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel : MainViewModel
 
+    private val updateBtn : Button
+    get() = findViewById(R.id.update_button_id)
+
+    private val factsView : TextView
+    get() = findViewById(R.id.textView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mainViewModel = ViewModelProvider(this, MainViewModelFactory()).get(MainViewModel::class.java)
 
-        mainViewModel.factsLivedata.observe(this, Observer{
+        //Observer class method is automatically invoked when LiveData is updated
+        mainViewModel.factsLiveData.observe(this, Observer{
+            factsView.text = it
+        })
 
-
-
+        updateBtn.setOnClickListener(View.OnClickListener {
+            mainViewModel.updateLiveData("Ram Krishna")
         })
 
     }
--- app/src/main/java/com/bhaskar/livedata/MainViewModel.kt
+++ app/src/main/java/com/bhaskar/livedata/MainViewModel.kt
@@ -1,10 +1,18 @@
 package com.bhaskar.livedata
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    val factsLivedata = MutableLiveData<String>("This is a fact")
+    val factsLiveDataObject = MutableLiveData<String>("This is a fact")
+
+    val factsLiveData: LiveData<String>
+        get() = factsLiveDataObject
+
+    fun updateLiveData(s: String) {
+        factsLiveDataObject.value = s
+    }
 
 }
